286cb6c9faeaccd8d91a17533682bea18026adaa
Krishna <krishna.indi@gmail.com>
2021-02-22 09:13:53 +0200

    Solutions of part 1 (Unicafe) 1.6
    Modified solutions of part 1.6 Unicafe step 1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,9 +7,48 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const clickFeedback = (feedback)=>{
+    switch (feedback) {
+      case 1:
+        setGood(good+1);
+        break;
+      case 2:
+          setNeutral(neutral+1);
+          break;
+      case 3:
+        setBad(bad+1);
+        break;
+    }
+  }
+
+
   return (
     <div>
-      code here
+      <div>
+        <h1>Give Feedback</h1>
+          <div>
+            <button onClick={() => clickFeedback(1)}>
+              good
+            </button>
+            <button onClick={() => clickFeedback(2)}>
+              neutral
+            </button>
+            <button onClick={() => clickFeedback(3)}>
+              bad
+            </button>
+          </div>
+      </div>
+
+      <div>
+        <h1>Statistics</h1>
+        <p>
+          Good {good}
+          <br/>
+          Neutral {neutral}
+          <br/>
+          Bad {bad}
+        </p>
+      </div>
     </div>
   )
 }
